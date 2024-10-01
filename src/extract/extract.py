b565160cd06760351e984346ef1950dcf0b9f62f
Paul Sandford <paul.sandford@bjss.com>
2024-10-01 16:16:32 +0100

    remove if name == main part from extract.py.

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -37,15 +37,3 @@ def extract_data(symbol, interval):
         raise Exception(
             f"Error fetching data for {symbol} at interval {interval}: {e}"
         ) from e
-
-
-if __name__ == "__main__":
-    dow_1min_data = extract_data("^DJI", "1m")
-    dow_15min_data = extract_data("^DJI", "15m")
-    dow_1hr_data = extract_data("^DJI", "1h")
-
-    BUCKET_NAME = "ingestion-bucket-st-trading-strategy-data-collection-pipeline"
-
-    save_to_s3_as_parquet(dow_1min_data, BUCKET_NAME)
-    save_to_s3_as_parquet(dow_15min_data, BUCKET_NAME)
-    save_to_s3_as_parquet(dow_1hr_data, BUCKET_NAME)
