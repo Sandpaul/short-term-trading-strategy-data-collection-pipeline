95445bad8e0dcdc2da527f8c80e5a856ce8b43f4
Paul Sandford <paul.sandford@bjss.com>
2024-10-02 17:57:27 +0100

    switch extract trigger to 5 minutes for testing purposes

--- terraform/extract_lambda.tf
+++ terraform/extract_lambda.tf
@@ -6,10 +6,16 @@ resource "aws_lambda_function" "extract_lambda" {
   role = aws_iam_role.extract_lambda_exec_role.arn
 }
 
+# resource "aws_cloudwatch_event_rule" "extract_daily_trigger" {
+#   name = "daily_lambda_trigger"
+#   description = "Triggers the extract Lambda function daily at midnight US Eastern Time"
+#   schedule_expression = "cron(0 5 * * ? *)"
+# }
+
 resource "aws_cloudwatch_event_rule" "extract_daily_trigger" {
   name = "daily_lambda_trigger"
   description = "Triggers the extract Lambda function daily at midnight US Eastern Time"
-  schedule_expression = "cron(0 5 * * ? *)"
+  schedule_expression = "rate(5 minutes)"
 }
 
 resource "aws_cloudwatch_event_target" "extract_lambda_trigger" {
