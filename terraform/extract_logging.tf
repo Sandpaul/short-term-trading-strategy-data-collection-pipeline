418948e811352e5b978f5ec85662190fe09b916d
Paul Sandford <paul.sandford@bjss.com>
2024-10-02 10:57:43 +0100

    remove duplicated cloudwatch permission.

--- terraform/extract_logging.tf
+++ terraform/extract_logging.tf
@@ -3,14 +3,6 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
   retention_in_days = 14
 }
 
-resource "aws_lambda_permission" "allow_cloudwatch_to_invoke_lambda" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.extract_lambda.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.extract_daily_trigger.arn
-}
-
 resource "aws_iam_role_policy_attachment" "lambda_logging" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.extract_lambda_exec_role.name
