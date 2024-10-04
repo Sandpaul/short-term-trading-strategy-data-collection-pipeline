3657a5e352c6f92d5a8bd66dc495286c8b7d81c7
Paul Sandford <paul.sandford@bjss.com>
2024-10-04 09:53:47 +0100

    move extract terraform files into seperate directory.

---
+++ terraform/extract/provider.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    region = "eu-west-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-bucket-trading-data-collection-pipeline"
+    key = "extract/terraform.tfstate"
+    region = "eu-west-2"
+    dynamodb_table = "lock-table-st-trading-data-collection-pipeline"
+    encrypt = true
+  }
+  
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.7.0"
+    }
+  }
+}
