18fab557fd4dd521002b7a185ddda8ae9e2c1958
Paul Sandford <paul.sandford@bjss.com>
2024-10-04 11:45:40 +0100

    reformat transform_adnd_save_data.

--- src/transform/transform_and_save_data.py
+++ src/transform/transform_and_save_data.py
@@ -9,9 +9,9 @@ def transform_and_save_data(event, context):
     logger.info("Lambda function triggered by S3 event")
     # Log the incoming event
     logger.info(f"Received event: {json.dumps(event)}")
-    
+
     # You can add additional processing here
-    
+
     return {
         'statusCode': 200,
         'body': json.dumps('Success! File processed.')
