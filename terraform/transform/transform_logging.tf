b33106916e63496447832ed66789c78e5c7f35e4
Paul Sandford <paul.sandford@bjss.com>
2024-10-04 12:08:55 +0100

    re-add transform logging to terraform.

--- terraform/transform/transform_logging.tf
+++ terraform/transform/transform_logging.tf
@@ -1,9 +1,9 @@
-# resource "aws_cloudwatch_log_group" "transform_lambda_log_group" {
-#   name = "/aws/lambda/${aws_lambda_function.transform_lambda.function_name}"
-#   retention_in_days = 14
-# }
+resource "aws_cloudwatch_log_group" "transform_lambda_log_group" {
+  name = "/aws/lambda/${aws_lambda_function.transform_lambda.function_name}"
+  retention_in_days = 14
+}
 
-# resource "aws_iam_role_policy_attachment" "transform_logging" {
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-#   role = aws_iam_role.transform_lambda_exec_role.name
-# }
+resource "aws_iam_role_policy_attachment" "transform_logging" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role = aws_iam_role.transform_lambda_exec_role.name
+}
